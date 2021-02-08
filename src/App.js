8ff9c3f4eecba325f4a01c579df37df36ea567be
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-02-08 14:15:51 -0500

    added basic styles to index.css; added error boundary component; set up Switch for react router in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import { Switch, Route } from "react-router-dom";
+import BoundaryError from './components/BoundaryError';
 function App() {
   return (
     <main className='App'>
-      
+      <BoundaryError>
+        <Switch>
+          
+        </Switch>
+      </BoundaryError>
     </main>
   );
 }

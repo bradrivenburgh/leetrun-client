7c1904f91b14c838a21dd31b9d5f217c5212cec0
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-02-11 11:43:04 -0500

    added minimal styling to landing page, nav, and footer

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./LandingPage.css";
 
 function LandingPage() {
   return (
@@ -13,67 +14,73 @@ function LandingPage() {
           </strong>
         </p>
       </header>
+      <div className='landing'>
+        <section>
+          <h2>A better way to track and share your progress</h2>
+          <p>
+            Spreadsheets and running journals have their place for tracking
+            progress, but LeetRun takes monitoring your progress to the next
+            level with dynamic summaries and stats tracking, as well as a
+            leaderboard to make it social.
+          </p>
+        </section>
 
-      <section>
-        <h2>A better way to track and share your progress</h2>
-        <p>
-          Spreadsheets and running journals have their place for tracking
-          progress, but LeetRun takes monitoring your progress to the next level
-          with dynamic summaries and stats tracking, as well as a leaderboard to
-          make it social.
-        </p>
-      </section>
+        <section>
+          <h2>Record your runs</h2>
+          <img
+            src='https://placehold.it/600x300?text=run%20recording%20interface'
+            alt='record run form'
+          />
+          <p>
+            LeetRun allows you to all the details about your runs and races,
+            including location, distance, race information, terrain type,
+            weather conditions, completion time, and notes.
+          </p>
+        </section>
 
-      <section>
-        <h2>Record your runs</h2>
-        <img
-          src='https://placehold.it/600x300?text=run%20recording%20interface'
-          alt='record run form'
-        />
-        <p>
-          LeetRun allows you to all the details about your runs and races,
-          including location, distance, race information, terrain type, weather
-          conditions, completion time, and notes.
-        </p>
-      </section>
+        <section>
+          <h2>Track your progress</h2>
+          <img
+            src='https://placehold.it/600x300?text=summary%20view'
+            alt='summary view'
+          />
+          <p>
+            View your PRs for common race distances, a chart of your run
+            frequency, badges for accomplishments, and sort and filter your all
+            your runs in once place.
+          </p>
+        </section>
 
-      <section>
-        <h2>Track your progress</h2>
-        <img
-          src='https://placehold.it/600x300?text=summary%20view'
-          alt='summary view'
-        />
-        <p>
-          View your PRs for common race distances, a chart of your run
-          frequency, badges for accomplishments, and sort and filter your all
-          your runs in once place.
-        </p>
-      </section>
+        <section>
+          <h2>Measure up against the competition</h2>
+          <img
+            src='https://placehold.it/600x300?text=leaderboards'
+            alt='leaderboard'
+          />
+          <p>
+            Make it social and see where your latest race PR lands you in the
+            rankings. LeetRun includes leaderboards that shows top times among
+            users for common race distances.
+          </p>
+        </section>
 
-      <section>
-        <h2>Measure up against the competition</h2>
-        <img
-          src='https://placehold.it/600x300?text=leaderboard'
-          alt='leaderboard'
-        />
-        <p>
-          Make it social and see where your latest race PR lands you in the
-          rankings. LeetRun includes a leaderboard that shows top times among
-          users for common race distances.
-        </p>
-      </section>
-
-      <section>
-        <h2>Start your running journey now</h2>
-        <button>View Demo</button>
-      </section>
+        <section>
+          <h2>Start your running journey now</h2>
+          <button>View Demo</button>
+        </section>
+      </div>
       <footer>
         <p>Created by Brad Rivenburgh</p>
         <p>
-          <a href='https://www.linkedin.com/in/brad-rivenburgh/'>LinkedIn</a> |{" "}
-          <a href='https://github.com/bradrivenburgh'>GitHub</a>
+          <a href='https://www.linkedin.com/in/brad-rivenburgh/' target='_new'>
+            LinkedIn
+          </a>{" "}
+          |{" "}
+          <a href='https://github.com/bradrivenburgh' target='_new'>
+            GitHub
+          </a>
         </p>
-        <p>Copyright &copy 2021</p>
+        <p>Copyright &copy; 2021</p>
       </footer>
     </>
   );

1b697e87b20f627460c73176506d50a957021c3e
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-02-11 13:45:47 -0500

    changed label element in CreateAccount to use htmlFor instead of for

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -9,19 +9,19 @@ function CreateAccount() {
       </header>
       <section className="create-account">
         <form action='#' className='create-account__form'>
-          <label for='first-name'>First Name:</label>
+          <label htmlFor='first-name'>First Name:</label>
           <input type='text' id='first-name' name='first-name' />
 
-          <label for='last-name'>Last Name:</label>
+          <label htmlFor='last-name'>Last Name:</label>
           <input type='text' id='last-name' name='last-name' />
 
-          <label for='email'>Email:</label>
+          <label htmlFor='email'>Email:</label>
           <input type='email' id='email' name='email' />
 
-          <label for='password'>Password:</label>
+          <label htmlFor='password'>Password:</label>
           <input type='password' id='password' name='password' />
 
-          <label for='verify-password'>Repeat Password:</label>
+          <label htmlFor='verify-password'>Repeat Password:</label>
           <input type='password' id='verify-password' name='verify-password' />
 
           <div className='create-account__buttons'>

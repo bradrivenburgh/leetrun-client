8b100d24f0e924a27501ede751caa25762046912
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-02-12 11:47:55 -0500

    run entries expand and collapse on click

--- src/components/SummaryEntry.js
+++ src/components/SummaryEntry.js
@@ -3,7 +3,7 @@ import "./SummaryEntry.css";
 
 function SummaryEntry() {
   const [expand, setExpand] = useState(false);
-  
+
   const handleClick = () => {
     setExpand(!expand);
     console.log(expand);
@@ -12,33 +12,31 @@ function SummaryEntry() {
   return (
     <>
       <section>
-        <div onClick={handleClick}>
-          <header>
-            <h3>Philadelphia, PA</h3>
-            <h3>01/01/2021</h3>
-          </header>
-          <div className={expand ? "collapse expand" : "collapse"}>
-            <p>
-              <strong>Distance:</strong> 5k
-            </p>
-            <p>
-              <strong>Time:</strong> 21:00
-            </p>
-            <p>
-              <strong>Weather:</strong> Clear
-            </p>
-            <p>
-              <strong>Surface:</strong> Pavement
-            </p>
-            <p>
-              <strong>Terrain:</strong> Flat
-            </p>
-            <p>
-              <strong>Notes: </strong>
-              Training run. Felt sluggish at the start, but my stride opened up
-              about 5 minutes in.
-            </p>
-          </div>
+        <header onClick={handleClick}>
+          <h3>Philadelphia, PA</h3>
+          <h3>01/01/2021</h3>
+        </header>
+        <div className={expand ? "collapse expand" : "collapse"}>
+          <p>
+            <strong>Distance:</strong> 5k
+          </p>
+          <p>
+            <strong>Time:</strong> 21:00
+          </p>
+          <p>
+            <strong>Weather:</strong> Clear
+          </p>
+          <p>
+            <strong>Surface:</strong> Pavement
+          </p>
+          <p>
+            <strong>Terrain:</strong> Flat
+          </p>
+          <p>
+            <strong>Notes: </strong>
+            Training run. Felt sluggish at the start, but my stride opened up
+            about 5 minutes in.
+          </p>
         </div>
         <div className='summary-entries__buttons'>
           <button>Edit</button>

0d1bf525f5f1cbbf3fb2f703e576974439faf31f
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-02-17 11:16:28 -0500

    added RunApiService with CRUD operations

---
+++ src/services/run-api-service.js
@@ -0,0 +1,70 @@
+import config from "../config";
+import TokenService from "./token-service";
+
+const RunApiService = {
+  getRuns() {
+    return fetch(`${config.API_ENDPOINT}/runs`, {
+      headers: {
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
+      }
+    })
+    .then(res =>
+      (!res.ok)
+        ? res.json().then(e => Promise.reject(e))
+        : res.json()
+    );
+},
+
+  postRun(newRun) {
+    return fetch(`${config.API_ENDPOINT}/runs`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
+      },
+      body: JSON.stringify({
+        newRun
+      })
+    })
+    .then(res =>
+      (!res.ok)
+        ? res.json().then(e => Promise.reject(e))
+        : res.json()
+    );
+  },
+
+  patchRun(run_id, updatedRun) {
+    return fetch(`${config.API_ENDPOINT}/runs/${run_id}`, {
+      method: 'PATCH',
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
+      },
+      body: JSON.stringify({
+        updatedRun
+      })
+    })
+    .then(res =>
+      (!res.ok)
+        ? res.json().then(e => Promise.reject(e))
+        : res.json()
+    );
+  },
+
+  deleteRun(run_id) {
+    return fetch(`${config.API_ENDPOINT}/runs/${run_id}`, {
+      method: 'DELETE',
+      headers: {
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+    .then(res =>
+      (!res.ok)
+        ? res.json().then(e => Promise.reject(e))
+        : res.json()
+    );
+
+  }
+}
+
+export default RunApiService;

44f2e79ccca3f9606362a8a6bffb5275836b9b54
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-03-02 15:37:02 -0500

    added tests confirming component renders required elements correctly

--- src/components/CreateAccount.test.js
+++ src/components/CreateAccount.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import CreateAccount from "./CreateAccount";
 
@@ -10,4 +10,31 @@ describe("CreateAccount", () => {
       </BrowserRouter>
     );
   });
+
+  test("contains a heading,'Sign-up'", () => {
+    render(
+      <BrowserRouter>
+        <CreateAccount />
+      </BrowserRouter>
+    );
+
+    screen.getByText('Sign-up'); 
+    screen.getByRole('heading');
+  })
+
+  test("contains a form with required fields", () => {
+    render(
+      <BrowserRouter>
+        <CreateAccount />
+      </BrowserRouter>
+    );
+
+    screen.getByLabelText(/user name/i);
+    screen.getByLabelText(/first name/i);
+    screen.getByLabelText(/last name/i);
+    screen.getByLabelText("Password:");
+    screen.getByLabelText(/repeat/i);
+  })
+
+  
 });

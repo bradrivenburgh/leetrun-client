d22131e08db7da912c53deee3dd6dd5387684dac
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-03-30 21:53:54 -0400

    added tests to EditRun.test.js

--- src/components/EditRun.test.js
+++ src/components/EditRun.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import EditRun from "./EditRun";
 
@@ -9,5 +9,43 @@ describe("EditRun", () => {
         <EditRun />
       </BrowserRouter>
     );
+  });
+
+  test("includes an 'Edit run' heading", () => {
+    render(
+      <BrowserRouter>
+        <EditRun />
+      </BrowserRouter>
+    );
+
+    const editRunHeading = screen.getByRole("heading", { name: /edit run/i });
+    expect(editRunHeading).toBeInTheDocument();
+  });
+
+  test("includes 'Date', 'Location', 'Distance', 'Time', 'Weather', 'Surface', 'Terrain', and 'Notes' fields", () => {
+    render(
+      <BrowserRouter>
+        <EditRun />
+      </BrowserRouter>
+    );
+
+    const date = document.getElementById("date");
+    expect(date).toBeInTheDocument();
+    const location = screen.getByRole("textbox", { name: /location/i });
+    expect(location).toBeInTheDocument();
+    const distance = screen.getByRole("spinbutton", { name: /distance/i });
+    expect(distance).toBeInTheDocument();
+    const time = screen.getAllByRole("spinbutton", { name: "" });
+    expect(time.length).toBe(3);
+    time.forEach((field) => expect(field).toBeInTheDocument());
+    const weather = screen.getAllByRole("radio");
+    expect(weather.length).toBe(3);
+    weather.forEach((field) => expect(field).toBeInTheDocument());
+    const surface = screen.getByRole("combobox", { name: /surface/i });
+    expect(surface).toBeInTheDocument();
+    const terrain = screen.getByRole("combobox", { name: /terrain/i });
+    expect(terrain).toBeInTheDocument();
+    const notes = screen.getByRole("textbox", { name: /notes/i });
+    expect(notes).toBeInTheDocument();
   });
 });

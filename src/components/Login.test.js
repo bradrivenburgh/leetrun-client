cf35bcd4c2ea1678bf3b55f6f76894084d328968
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-04-09 14:02:39 -0500

    added tests for Login.js

--- src/components/Login.test.js
+++ src/components/Login.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import Login from "./Login";
 
@@ -10,4 +10,58 @@ describe("Login", () => {
       </BrowserRouter>
     );
   });
+
+  test("contains a heading,'Sign-up'", () => {
+    render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
+
+    const heading = screen.getByRole("heading");
+    expect(heading).toHaveTextContent("Login");
+  });
+
+  test("contains a form with required input fields", () => {
+    render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
+
+    const labels = [
+      "User Name:",
+      "Password:",
+    ];
+
+    labels.forEach((label) => {
+      const element = screen.getByLabelText(`${label}`);
+      expect(element).toBeInTheDocument();
+    });
+  });
+
+
+  test("contains 'Submit' and 'Cancel' buttons", () => {
+    render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
+
+    const submitBtn = screen.getByRole("button", { name: "Submit" });
+    const cancelBtn = screen.getByRole("button", { name: "Cancel" });
+    expect(submitBtn).toBeInTheDocument();
+    expect(cancelBtn).toBeInTheDocument();
+  });
+
+  test("checks to make sure Submit button is disabled while required fields are empty", () => {
+    render(
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    );
+
+    expect(screen.getByRole("button", { name: "Submit" })).toBeDisabled();
+  });
+
 });
